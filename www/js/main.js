9116bbb7cd241f96cc3538c1d6435643a28fbadb
Dallas Kinzel <dkinzel@gmail.com>
2012-12-29 21:01:59 -0700

    updates

--- www/js/main.js
+++ www/js/main.js
@@ -8,25 +8,26 @@ function onAccelerometerSuccess(acceleration) {
     $('#popupMessage').html('Acceleration X: ' + acceleration.x + '\n' + 'Acceleration Y: ' + acceleration.y + '\n' + 'Acceleration Z: ' + acceleration.z + '\n' + 'Timestamp: ' + acceleration.timestamp + '\n');
     $('#popupMessage').popup('open');
 }
+
 function onAccelerometerFail() {
     $('#popupMessage').html('Accelerometer error');
     $('#popupMessage').popup('open');
 }
 
 function onDeviceReady(){
-    alert('deviceready');
-    $('#cameraButton').bind('tap', function () {alert('clicked camera');
-    navigator.camera.getPicture(onCameraSuccess, onCameraFail, { quality: 100, destinationType: Camera.DestinationType.FILE_URI});});
+    $('#cameraButton').bind('tap', function() {navigator.camera.getPicture(onCameraSuccess, onCameraFail, { quality: 100, destinationType: Camera.DestinationType.FILE_URI});});
 
-    $('#accelerometerButton').bind('tap', function () { alert('clicked accelerometer');
-    navigator.accelerometer.getCurrentAcceleration(onAccelerometerSuccess, onAccelerometerFail);})
+    $('#accelerometerButton').bind('tap', function() {navigator.accelerometer.getCurrentAcceleration(onAccelerometerSuccess, onAccelerometerFail);})
+        
+    alert('Device Name: ' + device.name     + '<br />' + 
+            'Device Cordova: ' + device.cordova + '<br />' + 
+            'Device Platform: ' + device.platform + '<br />' + 
+            'Device UUID: ' + device.uuid     + '<br />' + 
+            'Device Version: ' + device.version  + '<br />');
 }
 
 function init(){
-    alert('loading');
     document.addEventListener("deviceready", onDeviceReady,false);
-    //app.initialize();
-    alert('done loading');
 }
 
 
