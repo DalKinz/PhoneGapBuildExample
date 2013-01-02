eef68828a232e0b4c217fb8e8698c895b082370b
Dallas Kinzel <dkinzel@gmail.com>
2013-01-02 15:08:43 -0700

    updates

--- www/tests/tests-main.js
+++ www/tests/tests-main.js
@@ -1,6 +1,28 @@
+var phoneGapReady = false;
+var timeoutCounter = 0;
+
+function checkPhoneGapReady(){
+    
+}
+
+document.addEventListener("deviceready",function(){phoneGapReady = true},false);
+
+
+module('deviceIndependent');
 test('hello test', function() { ok(1 == '1','Passed!');});
 
+module('deviceDependent', {
+  setup: function() {
+      if (timeoutCounter > 4) return;
+      
+      while (!phoneGapReady){
+          timeoutCounter++
+          setTimeout(this.setup, 1000);
+      }
+  }
+});
 test('getPicture-Success', function() {
     getPicture();
     ok($('#cameraImage').attr('src') == '1','Passed!');
+    ok(true, 'once extra assert per test');
 });
