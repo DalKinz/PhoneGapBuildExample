cec73277536079ea11d7079a8a16fa9f983a4aba
Dallas Kinzel <dkinzel@gmail.com>
2013-01-02 15:18:37 -0700

    commented out the phonegapready code

--- www/tests/tests-main.js
+++ www/tests/tests-main.js
@@ -1,26 +1,22 @@
-var phoneGapReady = false;
-var timeoutCounter = 0;
+//var phoneGapReady = false;
+//var timeoutCounter = 0;
 
-function checkPhoneGapReady(){
-    
-}
-
-document.addEventListener("deviceready",function(){phoneGapReady = true},false);
+//document.addEventListener("deviceready",function(){phoneGapReady = true},false);
 
 
-module('deviceIndependent');
+//module('deviceIndependent');
 test('hello test', function() { ok(1 == '1','Passed!');});
 
-module('deviceDependent', {
-  setup: function() {
-      if (timeoutCounter > 4) return;
+//module('deviceDependent', {
+//  setup: function() {
+//      if (timeoutCounter > 4) return;
       
-      while (!phoneGapReady){
-          timeoutCounter++
-          setTimeout(this.setup, 1000);
-      }
-  }
-});
+//      while (!phoneGapReady){
+//          timeoutCounter++
+//          setTimeout(this.setup, 1000);
+//      }
+//  }
+//});
 test('getPicture-Success', function() {
     getPicture();
     ok($('#cameraImage').attr('src') == '1','Passed!');
